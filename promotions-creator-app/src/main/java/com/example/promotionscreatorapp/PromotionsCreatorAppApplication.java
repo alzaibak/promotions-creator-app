51bdcd39c8398b3a58879db3a36b4b05f442f411
alzaibak <alzaibak.abdalaziz@gmail.com>
2023-03-20 22:44:03 +0100

    spring boot application

---
+++ promotions-creator-app/src/main/java/com/example/promotionscreatorapp/PromotionsCreatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.promotionscreatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionsCreatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionsCreatorAppApplication.class, args);
+	}
+
+}
---
+++ promotions-creator-app/src/test/java/com/example/promotionscreatorapp/PromotionsCreatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promotionscreatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionsCreatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
