5cfab83d30a08ec8b40a2a12b62449f22c62d7df
alzaibak <alzaibak.abdalaziz@gmail.com>
2023-03-21 22:22:07 +0100

    login form service

---
+++ promotions-creator-app/src/main/java/com/example/promotionscreatorapp/service/LoginService.java
@@ -0,0 +1,35 @@
+package com.example.promotionscreatorapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.promotionscreatorapp.repository.LoginRepository;
+import com.example.promotionscreatorapp.model.LoginModel;
+
+@Service
+public class LoginService {
+	
+	@Autowired
+	private final LoginRepository loginRepository;
+
+	public LoginService(LoginRepository loginRepository) {
+		super();
+		this.loginRepository = loginRepository;
+	}
+
+	public LoginModel userRegistre(String email, String password) {
+		if (email != null && password!=null) {
+			LoginModel LoginModel = new LoginModel();
+			LoginModel.setEmail(email);
+			LoginModel.setPassword(password);
+			return loginRepository.save(LoginModel);
+		}
+		return null;
+	}
+	
+	public LoginModel userLogin(String email, String password) {
+		return loginRepository.findByEmailAndPassword(email,password).orElse(null);
+	}
+
+	
+
+}
