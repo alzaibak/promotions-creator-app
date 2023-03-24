fb5218000fe17d49461c01a93db21fe0a89d8c1a
alzaibak <alzaibak.abdalaziz@gmail.com>
2023-03-24 17:34:21 +0100

    All products PostgreSQL model

---
+++ promotions-creator-app/src/main/java/com/example/promotionscreatorapp/model/ProductsModel.java
@@ -0,0 +1,67 @@
+package com.example.promotionscreatorapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class ProductsModel {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	Integer id;
+	
+	String label;
+	String description;
+	Integer price;
+	String category;
+	String image;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	@Override
+	public String toString() {
+		return "ProductsModel [id=" + id + ", label=" + label + ", description=" + description + ", price=" + price
+				+ ", category=" + category + ", image=" + image + "]";
+	}
+
+
+}
